e9ad747e87d25b4e167a9b6c28e65f6f9856e934
Ana <a71350@ualg.pt>
2022-05-11 14:25:55 +0100

    pair

--- src/Pair.java
+++ src/Pair.java
@@ -1,6 +1,14 @@
 public class Pair extends Domino{
-
+    private Domino lat1, lat2;
     public Pair(int half){
-
+        this.half1 = half;
+        this.half2 = half;
+        this.x = 0;
+        this.y = 0;
+        this.side1 = null;
+        this.side2 = null;
+        this.lat1 = null;
+        this.lat2 = null;
+        this.flip = false;
     }
 }
