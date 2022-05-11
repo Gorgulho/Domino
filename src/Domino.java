c98ad7fc62e14263bd4d5fca53d1610b8ff71c86
Gorgulho <gorgulho.joao000@gmail.com>
2022-05-11 14:14:05 +0100

    first classes

---
+++ src/Domino.java
@@ -0,0 +1,28 @@
+public class Domino {
+    protected int x, y;
+    protected int half1, half2;
+    protected Domino side1, side2;
+    protected boolean flip;
+
+    public int getHalf1() {
+        return half1;
+    }
+
+    public int getHalf2() {
+        return half2;
+    }
+
+    public void flip(){
+
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
---
+++ src/Piece.java
@@ -0,0 +1,12 @@
+public class Piece extends Domino{
+
+    public Piece(int h1, int h2){
+        this.half1 = h1;
+        this.half2 = h2;
+        this.x = 0;
+        this.y = 0;
+        this.side1 = null;
+        this.side2 = null;
+        this.flip = false;
+    }
+}
