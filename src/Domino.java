50794964cf583479a8fcb6206f9ba240f8c97012
Gorgulho <gorgulho.joao000@gmail.com>
2022-05-19 15:39:54 +0100

    Domino changes

--- src/Domino.java
+++ src/Domino.java
@@ -1,22 +1,28 @@
 public class Domino implements Comparable<Domino>{
-    protected int x, y;
-    protected int half1, half2;
-    protected boolean rotated;
-    private final boolean isPair;
+    private int x, y;
+    private int half1, half2;
+    private boolean rotated;
+    private boolean isPair;
 
     public Domino(int half1, int half2){
-        this.isPair = half1 == half2;
+        if ((half1 >= 0 && half1 <= 6) && (half2 >= 0 && half2 <= 6)) {
+            this.isPair = half1 == half2;
 
-        this.half1 = half1;
-        this.half2 = half2;
-        this.rotated = false;
-        this.x = 0;
-        this.y = 0;
+            this.half1 = half1;
+            this.half2 = half2;
+            this.rotated = false;
+            this.x = 0;
+            this.y = 0;
+        } else System.exit(0);
     }
 
-    public int getHalf1() {return this.half1;}
+    public int getHalf1() {
+        return this.half1;
+    }
 
-    public int getHalf2() {return this.half2;}
+    public int getHalf2() {
+        return this.half2;
+    }
 
     public void rotate(){
         this.rotated = !this.rotated;
@@ -34,15 +40,25 @@ public class Domino implements Comparable<Domino>{
         this.y = y;
     }
 
+    /**
+     *
+     * @param p the object to be compared.
+     * @return 0 if the pieces are diferent and 1 if the pieces are the same
+     */
     @Override
     public int compareTo(Domino p) {
-            if((p.getHalf1() == this.getHalf1()) && (p.getHalf2() == this.getHalf2())) {
-                return 1;
-            }
-            return 0;
+        boolean firstComp = (p.getHalf1() == this.getHalf1()) && (p.getHalf2() == this.getHalf2());
+        boolean secondComp = (p.getHalf1() == this.getHalf2()) && (p.getHalf2() == this.getHalf1());
+        if(firstComp || secondComp) {
+            return 1;
+        }
+        return 0;
     }
 
-
+    /**
+     *
+     * @return a string representation of the object.
+     */
     @Override
     public String toString() {
         return "<" + this.half1 + "|" + this.half2 + ">";
