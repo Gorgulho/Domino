aa4c6e1c31a2b3c530a190c4dda1fd295d86e0a1
Ana <a71350@ualg.pt>
2022-05-20 21:56:09 +0100

    blablabla

--- src/Board.java
+++ src/Board.java
@@ -2,19 +2,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Board {
-    private Node firstDomino;
+    private final Node firstDomino;
     private int width; //2*28
     private int height; //3*28
     private List<Domino> dominos;
-
-    private LinkedList<Domino> corners;
-
     private static class Node{
         private Domino side1;
         private Domino side2;
         private Domino lat1;
         private Domino lat2;
         private final Domino piece;
+        private boolean isPair;
 
         /**
          *
@@ -23,33 +21,17 @@ public class Board {
         public Node(Domino piece){
             this.piece = piece;
         }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "side1=" + side1 +
-                    ", side2=" + side2 +
-                    ", lat1=" + lat1 +
-                    ", lat2=" + lat2 +
-                    ", piece=" + piece +
-                    '}';
-        }
     }
 
     /**
      *
-     * @param first - first Domino, in case <6|6>
-     * @param width - width limit for the board
-     * @param height - height limit for the board
+     * @param first //firstDomino
      */
     public Board(Domino first, int width, int height){
-        if (first.compareTo(new Domino(6, 6)) > 0) {
-            this.firstDomino = new Node(first);
-            this.width = width;
-            this.height = height;
-            this.dominos = new LinkedList<Domino>(); //Tomás isto serve para quê??
-            this.corners = new LinkedList<>();
-        } else System.exit(0);
+        this.firstDomino = new Node(first);
+        this.width = width;
+        this.height = height;
+        this.dominos = new LinkedList<Domino>();
     }
 
     /**
@@ -60,10 +42,9 @@ public class Board {
 
     /**
      *
-     * @param piece
-     * @param Corner
+     * @param d
      */
-    public void addDominoToCorner(Domino piece, Domino Corner){
+    public void addDomino(Domino d){
 
     }
 
@@ -72,16 +53,6 @@ public class Board {
      * @return
      */
     @Override
-    public String toString() {
-        return "Board{" +
-                "firstDomino=" + firstDomino +
-                ", width=" + width +
-                ", height=" + height +
-                ", dominos=" + dominos +
-                '}';
-    }
-
-    public LinkedList<Domino> getCorners() {
-        return corners;
+    public String toString() {return super.toString();
     }
 }
