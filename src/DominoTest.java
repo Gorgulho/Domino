9af2b96d7f29e8a95338a4dc7dad14c438b79eda
tomas <a71280@ualg.pt>
2022-05-24 19:26:17 +0100

    Board Test

--- src/DominoTest.java
+++ src/DominoTest.java
@@ -5,19 +5,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class DominoTest {
 
     @Test
-    void compareTo() {
-        assertEquals(0, new Piece(1, 2).compareTo(new Piece(2, 1)));
-        assertEquals(-1, new Piece(3, 2).compareTo(new Piece(2, 1)));
-        assertEquals(-1, new Pair(3).compareTo(new Pair(2)));
+    void testcompareTo() {
+        assertEquals(1, new Piece(1, 2).compareTo(new Piece(2, 1)));
+        assertEquals(0, new Piece(3, 2).compareTo(new Piece(2, 1)));
+        assertEquals(0, new Pair(3).compareTo(new Pair(2)));
     }
 
     @Test
-    void getHalf1() {
+    void testgetHalf1() {
         assertEquals(2, new Piece(2, 3).getHalf1());
     }
 
     @Test
-    void getHalf2() {
+    void testgetHalf2() {
         assertEquals(3, new Piece(2, 3).getHalf2());
+    }
+
+    @Test
+    void testisRotated(){
+        Domino a = new Piece(4, 5);
+        assertFalse(a.isRotated());
+        a.rotate();
+        assertTrue(a.isRotated());
+    }
+
+    @Test
+    void testisPair(){
+        Domino a = new Pair(4);
+        Domino b = new Piece(5, 4);
+        assertTrue(a.isPair());
+        assertFalse(b.isPair());
+    }
+
+    @Test
+    void testsumPoints(){
+        Domino a = new Pair(6);
+        Domino b = new Piece(0, 0);
+        assertEquals(12, a.sumPoints());
+        assertEquals(0, b.sumPoints());
     }
 }
