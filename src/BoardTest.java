03debe5b7cd65bfe7a5dd48e7232841b93f915e1
Gorgulho <gorgulho.joao000@gmail.com>
2022-05-24 15:54:42 +0100

    boardTest

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -5,9 +5,15 @@ class BoardTest {
     @Test
     void getFirstDomino(){
         Domino first = new Pair(6);
-        Board a = new Board(first);
+        Board a = new Board(first,57, 85);
         assertEquals(6, a.getFirstDomino().getHalf1());
         assertEquals(6, a.getFirstDomino().getHalf2());
     }
 
+    //@Test
+    /*public void testfirstDomino() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Board(new Domino(5, 4), 56, 84);
+        });*/
+    //}
 }
